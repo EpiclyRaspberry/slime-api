a5632b646655c59e321e4a2cd5653da5fe4053d6
Zeuroux <127013771+Zeuroux@users.noreply.github.com>
2023-05-21 13:04:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.use(express.json());
 
 app.post('/api/slime', (req, res) => {
   const tables = req.body.tables;
-  if (!Array.isArray(tables)) {
-    return res.status(400).json({ error: 'Invalid input' });
-  }
+ 
   
   const results = slime(tables);
   res.json({ results });
