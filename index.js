cafd9f6c9d6763e13c92a2aa16ec6d2ce9517dd1
Zeuroux <127013771+Zeuroux@users.noreply.github.com>
2023-05-21 12:19:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.post('/api/slime', (req, res) => {
   res.json({ results });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/slime', (req, res) => {
+  res.sendFile(path.join(__dirname, 'slime', 'index.html'));
 });
 
 app.listen(port, () => {
